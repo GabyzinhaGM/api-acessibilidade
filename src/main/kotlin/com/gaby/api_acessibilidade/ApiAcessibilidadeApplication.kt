27a7a2be33abbfea51143985ea380a02792f1d89
Gabriela Godoy Marques <gabyzinha.ggm@gmail.com>
2026-03-05 17:25:08 -0300

    Criando projeto com spring initializr

---
+++ src/main/kotlin/com/gaby/api_acessibilidade/ApiAcessibilidadeApplication.kt
@@ -0,0 +1,11 @@
+package com.gaby.api_acessibilidade
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiAcessibilidadeApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiAcessibilidadeApplication>(*args)
+}
---
+++ src/test/kotlin/com/gaby/api_acessibilidade/ApiAcessibilidadeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gaby.api_acessibilidade
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiAcessibilidadeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
